e90482cb972885fe73b19c8308553800b3c74af5
kapil jhajhria <jhajhria44@gmail.com>
2019-12-04 09:18:49 +0530

    somewhat working

--- bin/main.dart
+++ bin/main.dart
@@ -8,5 +8,30 @@
 // Example:
 // Input: [0,1,0,2,1,0,1,3,2,1,2,1]
 // Output: 6
+
+int waterCollected(List<int> pipe) {
+  List<int> totalWater = [];
+  for (int i = 0; i < pipe.length; i++) {
+    int count =0;
+    if(pipe[i] == 0){
+      continue;
+    }
+    for (int j = i + 1; j < pipe.length; j++) {
+      count++;
+      if ((pipe[j] >= pipe[i])) {
+        int area =count;
+        for(int k=i;k<j;k++){
+          area = area -pipe[k];
+        }
+        totalWater.add(area);
+        break;
+      }
+    }
+  }
+  return totalWater.fold(0, (previous, current) => previous + current);
+
+}
+
 main() {
+  print(waterCollected([0,1,0,2,1,0,1,3,2,1,2,1]));
 }
--- test/day_44_morning_challenge_test.dart
+++ test/day_44_morning_challenge_test.dart
@@ -1,6 +1,8 @@
 import 'package:test/test.dart';
-
+import '../bin/main.dart';
 void main() {
   test('Test Case 1', () {
+    expect(waterCollected([0,1,0,2,1,0,1,3,2,1,2,1]), 6);
+    expect(waterCollected( [ 1,2,0,3,1,2,0,1]), 4);
   });
 }
