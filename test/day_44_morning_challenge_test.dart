ce48ddeb9a5dd27b3d7983b43742ea5d1ffa3949
kapil jhajhria <jhajhria44@gmail.com>
2019-12-05 06:07:18 +0530

    added more test cases and also logic for empty list

--- test/day_44_morning_challenge_test.dart
+++ test/day_44_morning_challenge_test.dart
@@ -5,11 +5,18 @@ void main() {
   test('Test Case 1', () {
     expect(waterCollected([1, 0, 0, 1]), 2);
     expect(waterCollected([0, 0, 0, 0]), 0);
+    expect(waterCollected([1, 5]), 0);
+    expect(waterCollected([5]), 0);
+    expect(waterCollected([0]), 0);
     expect(waterCollected([2, 2, 2, 2]), 0);
     expect(waterCollected([1, 2, 3, 4]), 0);
     expect(waterCollected([4, 3, 2, 1]), 0);
     expect(waterCollected([2, 0, 1, 0, 2]), 5);
+    expect(waterCollected([0,2, 0, 1, 0, 2,0]), 5);
+    expect(waterCollected([1,0,2, 0, 1, 0, 2,0,1]), 7);
     expect(waterCollected([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
     expect(waterCollected([1, 2, 0, 3, 1, 2, 0, 1]), 4);
+    expect(waterCollected([2,1,0,3]), 3);
+    expect(waterCollected([]), 0);
   });
 }
